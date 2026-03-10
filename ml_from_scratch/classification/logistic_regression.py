Refactor classification algorithms: update Naive Bayes documentation, enhance KNN and Softmax implementations, and fix gradient calculation in Logistic Regression

ml_from_scratch/classification/logistic_regression.py
@@ -22,8 +22,8 @@ class LogisticRegression(Classifier) :
         for _ in range(self.epoch):
             z = X @ self.M
             prediction = np.where(z>=0,1/(1+np.exp(-z)),np.exp(z)/(1+np.exp(z)))
-            error = prediction - y + 2 * self.l1 * self.M + self.l2 * np.sign(self.M) 
-            gradiant = 1/n *  (X.T @ error)
+            error = prediction - y 
+            gradiant = 1/n *  (X.T @ error) + 2 * self.l2 * self.M + self.l1 * np.sign(self.M)
             self.M = self.M- self.lr * gradiant
 
     def predict(self,X):

ml_from_scratch/classification/softmax_regression.py
@@ -7,7 +7,7 @@ class Softmax :
         self.l1 = l1
         self.l2 = l2
         
-    def softmax(z):
+    def softmax(self,z):
         z = z - np.max(z,axis=1,keepdims=True)
         exp_z = np.exp(z)
         return exp_z/np.sum(exp_z,axis=1,keepdims=True)
@@ -27,8 +27,8 @@ class Softmax :
         for _ in range(self.epoch):
             z = X @ self.M
             prediction = self.softmax(z)
-            error = prediction - y + 2 * self.l1 * self.M + self.l2 * np.sign(self.M)
-            gradiant = 1/n *  (X.T @ error)
+            error = prediction - Y 
+            gradiant = 1/n *  (X.T @ error) + 2 * self.l2 * self.M + self.l1 * np.sign(self.M)
             self.M = self.M- self.lr * gradiant
 
     def predict(self,X):
@@ -36,4 +36,4 @@ class Softmax :
         X = np.hstack((X,ones))
         z = X @ self.M
         prediction = self.softmax(z)
-        return (prediction>=0.5).astype(int)
+        return np.argmax(prediction, axis=1)